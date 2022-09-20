321b86a6e7addef2ac9644420547a5d7c6bb3359
YouGiovanni <a3521110570@alumno.uttehuacan.edu.mx>
2022-09-19 23:29:20 -0500

    VUEComidarapida

---
+++ src/App.vue
@@ -0,0 +1,286 @@
+<template>
+  <!-- CSS only -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+
+  <div class="header">
+    <header>
+      <div class="back"></div>
+      <nav>
+        <h1 class="Titulo">CHAMX</h1>
+        <a href="#">Menú</a>
+        <a href="#">Servicio</a>
+        <a href="#">Promociones</a>
+        <a href="#">Tu cuenta</a>
+
+        <a href="#" class="nombre">Valencia Vazquez You Giovanni 4 "A"</a>
+      </nav>
+    </header>
+  </div>
+
+  <br>
+  <br>
+  <br>
+
+
+  <div class="container">
+    <div class="card-group mt-3">
+
+      <div class="card text-center border-info">
+        <div class="card-body">
+          <img
+            src="https://img.freepik.com/foto-gratis/vista-frontal-hamburguesa-stand_141793-15542.jpg?w=1060&t=st=1663644485~exp=1663645085~hmac=c856eea50f11174df7ad0ad2222c917c20303c0dcbac0dabfd35eb43caae44a9"
+            alt="" width="340">
+          <h4 class="card-title">Hamburguesa</h4>
+          <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam lectus sem,
+            tempor vitae mattis malesuada, ornare sed erat. Pellentesque nulla dui, congue
+            nec tortor sit amet, maximus mattis dui. </p>
+          <a href="#" class="btn btn-primary">Comprar $120.00</a>
+        </div>
+      </div>
+
+      <div class="card text-center border-info">
+        <div class="card-body">
+          <img
+            src="https://img.freepik.com/foto-gratis/pizza-hawaiana_1203-2455.jpg?w=1060&t=st=1663644665~exp=1663645265~hmac=34d79e037ce0ba4705c97214738ad7c5dad2a2081bfd0878069104cae7222830"
+            alt="" width="320">
+          <h4 class="card-title">Pizza</h4>
+          <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam lectus sem,
+            tempor vitae mattis malesuada, ornare sed erat. Pellentesque nulla dui, congue
+            nec tortor sit amet, maximus mattis dui. </p>
+          <a href="#" class="btn btn-primary">Comprar $220.0</a>
+        </div>
+      </div>
+
+      <div class="card text-center border-info">
+        <div class="card-body">
+          <img
+            src="https://img.freepik.com/foto-gratis/tres-arepas-delicioso-relleno_23-2148742378.jpg?w=1380&t=st=1663644764~exp=1663645364~hmac=87fa69fbba80e15f6970bc7d30840e8ae26ebc6b1bb2a000e6b4837e72d841fd"
+            alt="" width="320">
+          <h4 class="card-title">Empanadas</h4>
+          <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam lectus sem,
+            tempor vitae mattis malesuada, ornare sed erat. Pellentesque nulla dui, congue
+            nec tortor sit amet, maximus mattis dui. </p>
+          <a href="#" class="btn btn-primary">Comprar $80.00</a>
+        </div>
+      </div>
+
+
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="card-group mt-3">
+
+      <div class="card text-center border-info">
+        <div class="card-body">
+          <img
+            src="https://img.freepik.com/foto-gratis/toma-enfoque-selectivo-deliciosas-flautas-cerdo-al-estilo-mexicano-placa-blanca_181624-38905.jpg?w=1060&t=st=1663645042~exp=1663645642~hmac=60fbe6bcebc321ef4b256f35081776c6d4ea704c8010b46f6d375d26973403d0"
+            alt="" width="320">
+          <h4 class="card-title">Tostadas</h4>
+          <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam lectus sem,
+            tempor vitae mattis malesuada, ornare sed erat. Pellentesque nulla dui, congue
+            nec tortor sit amet, maximus mattis dui. </p>
+          <a href="#" class="btn btn-primary">Comprar $50.00</a>
+        </div>
+      </div>
+
+      <div class="card text-center border-info">
+        <div class="card-body">
+          <img
+            src="https://img.freepik.com/foto-gratis/concepto-comida-mexicana-alto-angulo_23-2148629376.jpg?w=1060&t=st=1663645258~exp=1663645858~hmac=9601a8ea3adf6f04c87f22c0db44af56e2b46d4447a76e9ca7fb1459f879796a"
+            alt="" width="320">
+          <h4 class="card-title">Tacos</h4>
+          <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam lectus sem,
+            tempor vitae mattis malesuada, ornare sed erat. Pellentesque nulla dui, congue
+            nec tortor sit amet, maximus mattis dui. </p>
+          <a href="#" class="btn btn-primary">Comprar $63.00</a>
+        </div>
+      </div>
+
+      <div class="card text-center border-info">
+        <div class="card-body">
+          <img
+            src="https://img.freepik.com/foto-gratis/comida-estilo-vida-spaghetti-foodie-gastronomia_1350-47.jpg?w=1060&t=st=1663645961~exp=1663646561~hmac=e568ac4cfed35d9f7aeb4b2f7d7ceefb882384e6df3f7277a841b85897fc2e24"
+            alt="" width="320">
+          <h4 class="card-title">Espagueti</h4>
+          <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam lectus sem,
+            tempor vitae mattis malesuada, ornare sed erat. Pellentesque nulla dui, congue
+            nec tortor sit amet, maximus mattis dui. </p>
+          <a href="#" class="btn btn-primary">Comprar $43</a>
+        </div>
+      </div>
+
+
+    </div>
+  </div>
+
+
+  <footer>
+    <div class="contenedor">
+        <h1>Gracias por comprar en VUEYGVV</h1>
+        <a href="">Servicio</a>
+        <a href="">Comunicate</a>
+        <a href="">Ubicación</a>
+        <a href="">Condición y Politica</a>
+    </div>
+  </footer>
+
+
+
+
+</template>
+
+<style>
+body {}
+
+header {
+  margin-top: 0px;
+
+}
+
+p {
+  text-align: center;
+}
+
+h1 {
+  text-align: center;
+  font-size: large;
+}
+
+.card {
+  display: inline-block;
+  padding: 10px 0px 50px 0px;
+  margin: 20px 40px 70px 40px;
+}
+
+.card-title {
+  text-align: center;
+}
+
+footer {
+  padding: 110px 0px 120px 10px;
+  margin-top: 20px;
+  background-color: rgb(0, 0, 0);
+
+}
+
+.contenedor h1 {
+  text-align: left;
+  color: #ffffff;
+}
+
+.contenedor a{
+  font-family: 'Oswald', sans-serif;
+  font-weight: 500;
+  text-transform: uppercase;
+  text-decoration: none;
+  color: #16151b;
+  margin: 0 15px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  position: relative;
+  display: inline-block;
+}
+
+
+@import url('https://fonts.googleapis.com/css?family=Oswald:500');
+
+.back {
+  position: relative;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background: #efeff1;
+  background-size: cover;
+}
+
+.thx {
+  position: fixed;
+  left: 50%;
+  transform: translateX(-50%);
+  bottom: 15px;
+}
+
+nav {
+  width: 100%;
+  position: relative;
+  top: 50px;
+  text-align: center;
+}
+
+nav h1 {
+  text-align: left;
+}
+
+nav a {
+  font-family: 'Oswald', sans-serif;
+  font-weight: 500;
+  text-transform: uppercase;
+  text-decoration: none;
+  color: #16151b;
+  margin: 0 15px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  position: relative;
+  display: inline-block;
+}
+
+
+nav a:before {
+  content: '';
+  position: absolute;
+  width: 100%;
+  height: 3px;
+  background: #16151b;
+  top: 47%;
+  animation: out 0.2s cubic-bezier(1, 0, 0.58, 0.97) 1 both;
+}
+
+nav a:hover:before {
+  animation: in 0.2s cubic-bezier(1, 0, 0.58, 0.97) 1 both;
+
+}
+
+@keyframes in {
+  0% {
+    width: 0;
+    left: 0;
+    right: auto;
+  }
+
+  100% {
+    left: 0;
+    right: auto;
+    width: 100%;
+  }
+}
+
+@keyframes out {
+  0% {
+    width: 100%;
+    left: auto;
+    right: 0;
+  }
+
+  100% {
+    width: 0;
+    left: auto;
+    right: 0;
+  }
+}
+
+@keyframes show {
+  0% {
+    opacity: 0;
+    transform: translateY(-10px);
+  }
+
+  100% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+</style>
